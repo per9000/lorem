adf3737ecb8874b90d54b7dfe801f74d47fc218c
Per Erik Strandberg <per9000@gmail.com>
2014-04-25 21:59:35 +0200

    Chars, words, lines, sentences all ok. Script for sanitycheck added.

---
+++ sanitycheck.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+#
+# just a sanity check
+#
+
+testcases=0
+broken=0
+
+for random in "" "--randomize"
+do
+    for lorem in lorem decameron faust fleurs spook strandberg
+    do
+        for n in 0 1 2 3 100 100000
+        do
+            action="lorem --$lorem -n $n $random"
+            if [ `$action | wc -w` -ne $n ]
+            then
+                echo BROKEN: $action
+                broken=$((broken+1))
+            else
+                echo OK: $action
+            fi
+            testcases=$((testcases+1))
+
+            action="lorem --$lorem -s $n $random"
+            if [ `$action | tr ";.\!?" "\n\n\n" | wc -l` -ne $((n+1)) ]
+            then
+                echo BROKEN: $action
+                broken=$((broken+1))
+            else
+                echo OK: $action
+            fi
+            testcases=$((testcases+1))
+
+            action="lorem --$lorem -c $n $random"
+            if [ `$action | wc -c` -ne $((n+1)) ]
+            then
+                echo BROKEN: $action
+                broken=$((broken+1))
+            else
+                echo OK: $action
+            fi
+            testcases=$((testcases+1))
+
+            # there will be at least one line - 0 makes a blank one
+            action="lorem --$lorem -l $n $random"
+            if [ $n -eq 0 ]
+            then
+                n=1
+            fi
+            if [ `$action | wc -l` -ne $n ]
+            then
+                echo BROKEN: $action
+                broken=$((broken+1))
+            else
+                echo OK: $action
+            fi
+            testcases=$((testcases+1))
+        done
+    done
+done
+
+echo $testcases test cases, $broken broken
